54d77c562c7d5a859fafd773058e871eb68fc46f
Zak Kriner <zak@zakak.net>
2019-05-21 17:20:03 -0400

    Add type hints to fix reflection warnings

--- src/net/zakak/clj_extjwnl.clj
+++ src/net/zakak/clj_extjwnl.clj
@@ -1,6 +1,12 @@
 (ns net.zakak.clj-extjwnl
-  (:import (net.sf.extjwnl.data POS)
-           net.sf.extjwnl.dictionary.Dictionary)
+  (:import (net.sf.extjwnl.data
+            IndexWord
+            Word
+            POS
+            Pointer
+            PointerType
+            Synset)
+           (net.sf.extjwnl.dictionary Dictionary))
   (:require [clojure.string :as str]))
 
 (def ^:private default-map-element-opts {:limit 1000})
@@ -58,62 +64,65 @@
 
 ;; ## Element types
 
-(defn- pos-element [pos el]
-  (elements (.getPOS el) pos))
-
 (defmethod -keyword-element :identity [_ el]
   el)
 
-(defmethod -keyword-element :pointer-type/label [_ el]
+(defmethod -map-element :index-word/pos [{:keys [index-word/pos]} ^IndexWord el]
+  (elements (.getPOS el) pos))
+
+(defmethod -map-element :index-word/senses [{:keys [index-word/senses opts]} ^IndexWord el]
+  (mapv #(elements % senses)
+        (take (:limit opts) (.getSenses el))))
+
+(defmethod -keyword-element :index-word/lemma [_ ^IndexWord el]
+  (.getLemma el))
+
+(defmethod -keyword-element :pointer-type/label [_ ^PointerType el]
   (.getLabel el))
 
-(defmethod -map-element :pointer/synset [{:keys [pointer/synset]} el]
+(defmethod -map-element :pointer/synset [{:keys [pointer/synset]} ^Pointer el]
   (elements (.getTargetSynset el)
             synset))
 
-(defmethod -map-element :pointer/type [{:keys [pointer/type]} el]
+(defmethod -map-element :pointer/type [{:keys [pointer/type]} ^Pointer el]
   (elements (.getType el)
             type))
 
-(defmethod -keyword-element :pos/label [_ el]
+(defmethod -keyword-element :pos/label [_ ^POS el]
   (.getLabel el))
 
-(defmethod -keyword-element :synset/gloss [_ el]
+(defmethod -keyword-element :synset/gloss [_ ^Synset el]
   (.getGloss el))
 
-(defmethod -map-element :synset/pointers [{:keys [synset/pointers opts]} el]
+(defmethod -map-element :synset/pointers [{:keys [synset/pointers opts]} ^Synset el]
   (mapv #(elements % pointers)
         (take (:limit opts) (.getPointers el))))
 
-(defmethod -map-element :synset/pos [{:keys [synset/pos]} el]
-  (pos-element pos el))
+(defmethod -map-element :synset/pos [{:keys [synset/pos]} ^Synset el]
+  (elements (.getPOS el) pos))
 
-(defmethod -map-element :synset/words [{:keys [synset/words opts]} el]
+(defmethod -map-element :synset/words [{:keys [synset/words opts]} ^Synset el]
   (mapv #(elements % words)
         (take (:limit opts) (.getWords el))))
 
-(defmethod -keyword-element :word/lemma [_ el]
+(defmethod -keyword-element :word/lemma [_ ^Word el]
   (.getLemma el))
 
-(defmethod -map-element :word/pos [{:keys [word/pos]} el]
-  (pos-element pos el))
-
-(defmethod -map-element :word/senses [{:keys [word/senses opts]} el]
-  (mapv #(elements % senses)
-        (take (:limit opts) (.getSenses el))))
+(defmethod -map-element :word/pos [{:keys [word/pos]} ^Word el]
+  (elements (.getPOS el) pos))
 
 ;; ## Main
 
 (defn lookup-all-index-words
   "Use dict to look up lemma using query"
-  [dict lemma query]
+  [^Dictionary dict lemma query]
   (->> (.lookupAllIndexWords dict lemma)
        (.getIndexWordCollection)
        (mapv #(elements % query))))
 
 (defn lookup-index-word
   "Use dict to look up lemma using pos and query"
-  [dict pos lemma query]
+  [^Dictionary dict pos lemma query]
   (elements (.lookupIndexWord dict
                               (or (POS/getPOSForLabel pos)
                                   (throw (Exception. (str "Unknown part of speech (pos): '" pos "'"))))
--- test/net/zakak/clj_extjwnl_test.clj
+++ test/net/zakak/clj_extjwnl_test.clj
@@ -1,52 +1,54 @@
 (ns net.zakak.clj-extjwnl-test
   (:require [clojure.test :refer [deftest is testing]]
-            [net.zakak.clj-extjwnl :refer :all]))
+            [net.zakak.clj-extjwnl :refer [default-dictionary
+                                           lookup-index-word
+                                           lookup-all-index-words]]))
 
 (def dict (default-dictionary))
 
 (def example-pointers-query
-  [:word/lemma
-   {:word/pos [:pos/label]}
-   {:word/senses [:synset/gloss
-                  {:synset/words [:word/lemma
-                                  {:word/pos [:pos/label]}]}
-                  {:synset/pos [:pos/label]}
-                  {:synset/pointers [{:pointer/type [:pointer-type/label]}
-                                     {:pointer/synset [:synset/gloss
-                                                       {:synset/pos [:pos/label]}
-                                                       {:synset/words [:word/lemma
-                                                                       {:word/pos [:pos/label]}]}]}]}]}])
+  [:index-word/lemma
+   {:index-word/pos [:pos/label]}
+   {:index-word/senses [:synset/gloss
+                        {:synset/words [:word/lemma
+                                        {:word/pos [:pos/label]}]}
+                        {:synset/pos [:pos/label]}
+                        {:synset/pointers [{:pointer/type [:pointer-type/label]}
+                                           {:pointer/synset [:synset/gloss
+                                                             {:synset/pos [:pos/label]}
+                                                             {:synset/words [:word/lemma
+                                                                             {:word/pos [:pos/label]}]}]}]}]}])
 
 (def example-senses-query
-  [:word/lemma
-   {:word/pos [:pos/label]}
-   {:word/senses [:synset/gloss
-                  {:synset/words [:word/lemma
-                                  {:word/pos [:pos/label]}]}
-                  {:synset/pos [:pos/label]}]}])
+  [:index-word/lemma
+   {:index-word/pos [:pos/label]}
+   {:index-word/senses [:synset/gloss
+                        {:synset/words [:word/lemma
+                                        {:word/pos [:pos/label]}]}
+                        {:synset/pos [:pos/label]}]}])
 
 (deftest lookup-index-word-test
-  (is (= {:word/lemma  "pit"
-          :word/pos    {:pos/label "noun"}
-          :word/senses [{:synset/words [{:word/lemma "pit"}]
-                         :synset/pos   {:pos/label "noun"}}]}
+  (is (= {:index-word/lemma  "pit"
+          :index-word/pos    {:pos/label "noun"}
+          :index-word/senses [{:synset/words [{:word/lemma "pit"}]
+                               :synset/pos   {:pos/label "noun"}}]}
          (lookup-index-word dict "noun" "pit"
-                            '[:word/lemma
-                              {:word/pos [:pos/label]}
-                              {(:word/senses :limit 1) [{(:synset/words :limit 1) [:word/lemma]}
-                                                        {:synset/pos [:pos/label]}] }]))))
+                            '[:index-word/lemma
+                              {:index-word/pos [:pos/label]}
+                              {(:index-word/senses :limit 1) [{(:synset/words :limit 1) [:word/lemma]}
+                                                              {:synset/pos [:pos/label]}] }]))))
 
 (deftest lookup-all-index-words-test
-  (is (= [{:word/lemma  "pit"
-           :word/pos    {:pos/label "noun"}
-           :word/senses [{:synset/words [{:word/lemma "pit"}]
-                          :synset/pos   {:pos/label "noun"}}]}
-          {:word/lemma  "pit"
-           :word/pos    {:pos/label "verb"}
-           :word/senses [{:synset/words [{:word/lemma "pit"}]
-                          :synset/pos   {:pos/label "verb"}}]}]
+  (is (= [{:index-word/lemma  "pit"
+           :index-word/pos    {:pos/label "noun"}
+           :index-word/senses [{:synset/words [{:word/lemma "pit"}]
+                                :synset/pos   {:pos/label "noun"}}]}
+          {:index-word/lemma  "pit"
+           :index-word/pos    {:pos/label "verb"}
+           :index-word/senses [{:synset/words [{:word/lemma "pit"}]
+                                :synset/pos   {:pos/label "verb"}}]}]
          (lookup-all-index-words dict "pit"
-                                 '[:word/lemma
-                                   {:word/pos [:pos/label]}
-                                   {(:word/senses :limit 1) [{(:synset/words :limit 1) [:word/lemma]}
-                                                             {:synset/pos [:pos/label]}] }]))))
+                                 '[:index-word/lemma
+                                   {:index-word/pos [:pos/label]}
+                                   {(:index-word/senses :limit 1) [{(:synset/words :limit 1) [:word/lemma]}
+                                                                   {:synset/pos [:pos/label]}] }]))))
