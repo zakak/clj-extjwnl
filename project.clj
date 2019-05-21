9052a86ef014d67b1720243cf8ae6b2f5469748b
Zak Kriner <zak@zakak.net>
2019-05-21 17:28:21 -0400

    Upgrade clojure, extjwnl dependencies

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License - v 2.0"
             :url "https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.txt"}
   :global-vars {*warn-on-reflection* true}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [net.sf.extjwnl/extjwnl "1.9.3"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [net.sf.extjwnl/extjwnl "2.0.2"]
                  [net.sf.extjwnl/extjwnl-data-wn31 "1.2"]])
