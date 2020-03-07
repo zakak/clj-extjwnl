d3ff4f354521b48ec02e0c72529c5d64e8dde137
Zak Kriner <zak@zakak.net>
2020-03-07 12:54:52 -0500

    Update version to 0.1.2-SNAPSHOT

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## [0.1.2-SNAPSHOT] - 2020-03-07
+
+- Change lookup-all-index-words to lookup
+- Change lookup-index-word to lookup-by-pos
+
 ## [0.1.1-SNAPSHOT] - 2019-05-21
 
 - Add type hints
--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject net.zakak/clj-extjwnl "0.1.1-SNAPSHOT"
+(defproject net.zakak/clj-extjwnl "0.1.2-SNAPSHOT"
   :description "Clojure wrapper for extJWNL (Extended Java WordNet Library) Java API."
   :url "https://github.com/zakak/clj-extjwnl"
   :license {:name "Eclipse Public License - v 2.0"
             :url "https://www.eclipse.org/org/documents/epl-2.0/EPL-2.0.txt"}
+  :scm {:name "git" :url "https://github.com/zakak/clj-extjwnl"}
   :global-vars {*warn-on-reflection* true}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [net.sf.extjwnl/extjwnl "2.0.2"]
